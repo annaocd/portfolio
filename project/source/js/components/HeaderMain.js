bd1c175148e2ff0216a551ffcf1fae40b6ba2cc7
anna yovandich <anna.yovandich@ff0000.com>
2017-04-13 21:08:49 -0700

    updated HeaderMain design

--- project/source/js/components/HeaderMain.js
+++ project/source/js/components/HeaderMain.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router'
+import cx from 'bem-classnames'
 
 import '../../stylesheets/components/header-main.scss'
 
 function HeaderMain (props) {
+  let classes = {
+      name: 'header-main',
+      state: ['top']
+  }
+
   return (
-    <header className='header-main'>
+    <header className={cx(classes, { top: props.isTop })}>
+    <Link to='/'>
       <h1>
-        <Link to='/'>
-          <span>a<i>n</i>na</span>
-          <span>Yovandich</span>
-        </Link>
+        <span>а<i>n</i>na</span>
+        <span>Yovandich</span>
       </h1>
+    </Link>
     </header>
   )
 }
