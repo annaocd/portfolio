0e1ee4868198ef253047de51b07780f21b42c186
anna yovandich <anna.yovandich@ff0000.com>
2017-04-22 18:42:28 -0700

    separated <h1> into Logo component

---
+++ project/source/js/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../stylesheets/components/logo.scss'
+
+function Logo (props) {
+  return (
+    <h1 className='logo'>
+      <span>а<i>n</i>na</span>
+      <span>Yovandich</span>
+    </h1>
+  )
+}
+
+export default Logo
