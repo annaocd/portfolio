ce42446f387fd6ed375324c6c07cc9774e33acb1
anna yovandich <anna.yovandich@ff0000.com>
2017-04-24 16:10:33 -0700

    corrected AspectRatio formula

--- project/source/js/components/AspectRatio.js
+++ project/source/js/components/AspectRatio.js
@@ -2,16 +2,30 @@ import React from 'react'
 import cx from 'bem-classnames'
 import '../../stylesheets/components/aspect-ratio.scss'
 
+function calculateRatio (width, height) {
+  return 100 / (width / height)
+}
+
+function getImageSize (image) {
+  const img = new Image()
+  img.src = image
+  return [img.width, img.height]
+}
+
 function AspectRatio (props) {
   let classes = {
       name: 'aspect-ratio ' + props.className,
       states: ['absolute', 'exiting', 'pressed']
   }
 
+  const imgSize = getImageSize(props.image)
+  const imgWidth = imgSize[0]
+  const imgHeight = imgSize[1]
+
   //TODO: make backgroundImage conditional per props.image
   return (
     <div className={cx(classes, { absolute: props.isAbsoluteParent })}>
-      <span className='aspect-ratio__spacer' style={{ paddingTop: props.ratio + '%'}}></span>
+      <span className='aspect-ratio__spacer' style={{ paddingBottom: calculateRatio(imgWidth, imgHeight) + '%'}}></span>
       <div className='aspect-ratio__content' style={props.image && { backgroundImage: 'url(' + props.image + ')'}}>
         {props.children}
       </div>
