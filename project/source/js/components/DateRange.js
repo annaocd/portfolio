9aea5aedf9746d1b98568b8916b663f93c395449
anna yovandich <anna@stackery.io>
2020-08-25 22:59:50 -0700

    Allow undefined dateEnd

--- project/source/js/components/DateRange.js
+++ project/source/js/components/DateRange.js
@@ -7,18 +7,27 @@ function DateRange (props) {
 
   const startMonth = dateStart.split(' ')[0]
   const startYear = dateStart.split(' ')[1]
-  const endMonth = dateEnd.split(' ')[0]
-  const endYear = dateEnd.split(' ')[1]
-
+  const endMonth = dateEnd ? dateEnd.split(' ')[0] : null
+  const endYear = dateEnd ? dateEnd.split(' ')[1] : null
 
   return (
     <span className='date-range'>
       <span className='date-range__month'>{startMonth}</span>
       <span className='date-range__year'>{startYear}</span>
-      <span className='date-range__month'>{endMonth}</span>
-      <span className='date-range__year'>{endYear}</span>
+      {endMonth &&
+        <span className='date-range__month'>{endMonth}</span>
+      }
+
+      {endYear &&
+        <span className='date-range__year'>{endYear}</span>
+      }
+
+      {!dateEnd &&
+        <span>Present</span>
+      }
+
     </span>
-  )
+  );
 }
 
 export default DateRange
